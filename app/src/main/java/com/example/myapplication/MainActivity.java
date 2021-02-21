9990e4dd4fc35fd0eebb9d9ee72c0911f04d49c6
Mirdan <akinovmirdan@gmail.com>
2021-02-22 00:12:52 +0600

    Added: - Buttons in UI - findViewbyID

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,40 +6,49 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
-    Button button;
-    EditText editText;
+    private TextView tvInput, tvOutput;
+    private Button button1, button2, button3, button4, button5, button6, button7, button8, button9,
+            button0, buttonMinus, buttonPlus, buttonEquals, buttonClear;
+    String process;
+    boolean checkBracket = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.text_view);
-        textView.setText("Today test");
-        button = findViewById(R.id.button1);
-        editText = findViewById(R.id.edit_text);
+        findViews();
 
-        button.setOnClickListener(new View.OnClickListener() {
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                process = tvInput.getText().toString();
 
-                String text = editText.getText().toString();
-                textView.setText(text);
                 Log.d("ololo", "Clicked");
             }
         });
+    }
 
+    private void findViews() {
+        tvInput = findViewById(R.id.text_view1);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
+        button6 = findViewById(R.id.button6);
+        button7 = findViewById(R.id.button7);
+        button8 = findViewById(R.id.button8);
+        button9 = findViewById(R.id.button9);
+        button0 = findViewById(R.id.button0);
+        buttonPlus = findViewById(R.id.buttonPlus);
+        buttonMinus = findViewById(R.id.buttonMinus);
+        buttonEquals = findViewById(R.id.buttonEquals);
+        buttonClear = findViewById(R.id.buttonClear);
+        tvOutput = findViewById(R.id.text_view2);
     }
 }
