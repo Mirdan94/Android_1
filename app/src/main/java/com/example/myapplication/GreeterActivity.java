b80f8b4f4f59c009dd7ad34638f6e5ab7472c42b
Mirdan <akinovmirdan@gmail.com>
2021-03-17 16:04:15 +0600

    Intents

--- app/src/main/java/com/example/myapplication/GreeterActivity.java
+++ app/src/main/java/com/example/myapplication/GreeterActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,5 +18,20 @@ public class GreeterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_greeter);
+
+        nameField = findViewById(R.id.name_field);
+        saveButton = findViewById(R.id.save_button);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameField.getText().toString();
+
+                Intent intent = new Intent(GreeterActivity.this, IntentActivity.class);
+                intent.putExtra("key", name);
+                startActivity(intent);
+
+            }
+        });
     }
 }
